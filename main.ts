9cdb530934e7ece42d1fa0d80917e75370523902
oyunverobotik <78992206+oyunverobotik@users.noreply.github.com>
2021-02-13 09:08:53 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,26 @@
+function showNumber2 (num2: number) {
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `).showImage(0)
+    column = num2 / 5
+    row = num2 % 5
+    for (let i = 0; i <= row; i++) {
+        for (let j = 0; j <= column; j++) {
+            led.plot(i, j)
+        }
+    }
+}
 input.onButtonPressed(Button.A, function () {
     num = num - 1
-    showNumber(num)
+    showNumber2(num)
 })
 input.onButtonPressed(Button.B, function () {
     num = num + 1
-    showNumber(num)
+    showNumber2(num)
 })
 function showNumber (num2: number) {
     column = num2 / 5
@@ -15,3 +31,7 @@ let row = 0
 let column = 0
 let num = 0
 num = -1
+led.plotBarGraph(
+0,
+2
+)
