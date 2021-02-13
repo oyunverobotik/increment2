6e9f16d24122b6055867c8f404c5b3848b9bb15d
oyunverobotik <78992206+oyunverobotik@users.noreply.github.com>
2021-02-13 05:59:55 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    num = num - 1
+    showNumber(num)
 })
+input.onButtonPressed(Button.B, function () {
+    num = num + 1
+    showNumber(num)
+})
+function showNumber (num: number) {
+    basic.showString("" + (num))
+}
+let num = 0
+num = 0
